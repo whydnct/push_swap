cb3fdd009ea21c186e794147aed8c8bb72eecb74
aperez-m <aperez-m@student.42urduliz.com>
2023-01-15 17:41:15 +0100

    fix variable types

--- push_swap.h
+++ push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aperez-m <aperez-m@student.42urduliz.com>  +#+  +:+       +#+        */
+/*   By: aperez-m <aperez-m@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 13:18:06 by aperez-m          #+#    #+#             */
-/*   Updated: 2023/01/15 14:37:04 by aperez-m         ###   ########.fr       */
+/*   Updated: 2023/01/15 17:28:06 by aperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,18 @@
 # define PUSH_SWAP_H
 
 # include <unistd.h>
-# include 
+# include <stdlib.h>
+
+
+void	push_a(int **stack_b, int **stack_a, int *size_b, int *size_a);
+void	push_b(int **stack_a, int **stack_b, int *size_a, int *size_b);
+void	r_rotate_a(int **stack, int *size, int in_both_call);
+void	r_rotate_b(int **stack, int *size, int in_both_call);
+void	r_rotate_both(int **stack_a, int **stack_b, int *size_a, int *size_b);
+void	rotate_a(int **stack, int *size, int in_both_call);
+void	rotate_b(int **stack, int *size, int in_both_call);
+void	rotate_both(int **stack_a, int **stack_b, int *size_a, int *size_b);
+void	add_on_top(int **stack_to, int **stack_from, int *size_to);
+void	remove_from_top(int **stack, int *size);
+
+#endif
