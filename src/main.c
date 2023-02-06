d3c794f5a9647ff9b0e56c6c7513ca5cae32a858
al <whydnct@gmail.com>
2023-02-06 21:13:34 +0100

    .

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aperez-m <aperez-m@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 12:37:37 by aperez-m          #+#    #+#             */
-/*   Updated: 2023/02/05 21:09:44 by aperez-m         ###   ########.fr       */
+/*   Updated: 2023/02/06 21:12:42 by aperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,28 +25,25 @@ int	check_argv(char **argv)
 	return (1);
 }
 */
-void	read_list(int argc, char **argv, t_list **stack_a)
+void	read_list(int argc, int *contents, t_list **stack_a)
 {
-	int		content;
 	int		i;
-	t_list	*new;
 
-	i = 1;
-	while (i < argc)
+	i = 0;
+	while (i < argc - 1)
 	{
-		content = ft_atoi(argv[i]);
-		new = ft_lstnew(&content);
-		printf("%p\n", new->next);
-		ft_lstadd_front(stack_a, new);
+		ft_lstadd_front(stack_a, ft_lstnew(contents + i));
 		i++;
-		printf("%d\n", *(int *)((**stack_a).content));
-		//printf("%p\n", ft_lstlast(*stack_a)->next);
+		printf("value:%d\n", *(int *)((**stack_a).content));
+		printf("value address:%p\n", (**stack_a).content);
+		printf("element address:%p\n", *stack_a);
 	}
 }
 
 void	ft_del(void *content)
 {
 	free(content);
+//	*(int*)content = 0;
 }
 
 void	ft_print_list(void *stack_a_el)
@@ -59,12 +56,20 @@ int	main(int argc, char **argv)
 {
 	t_list	**stack_a;
 	//t_list	**stack_b;
+	int		*contents;
+	int		i;
 
 	//check_argv(argv);
+	contents = malloc(sizeof(int)*(argc-1));
+	i = 0;
+	while(++i < argc)
+		contents[i - 1] = ft_atoi(argv[i]);
 	stack_a = malloc(sizeof(t_list **));
-	read_list(argc, argv, stack_a);
+	read_list(argc, contents, stack_a);
 //	push_b(stack_a, stack_b);
 //	push_swap(stack_a, stack_b);
-	ft_lstiter(*stack_a, &ft_print_list);
+//	printf("%d\n",*(int *)(**stack_a).content);
+//	ft_lstiter(*stack_a, &ft_print_list);
 	ft_lstclear(stack_a, &ft_del);
+	free(contents);
 }
